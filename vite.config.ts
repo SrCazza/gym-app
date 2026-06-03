1d39f824777d189641c6a0e0d26082ae1a30686e
MrGown <38081022+SrCazza@users.noreply.github.com>
2026-06-03 18:04:32 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  nitro: {
-    preset: "vercel",
-    output: {
-      dir: ".vercel/output",
-    },
-  },
 });
